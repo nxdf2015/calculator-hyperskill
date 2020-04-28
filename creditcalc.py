fb312c862d63f2e5352d1e4fc7b0942e8cea25bc
xavier.niquet <niquet_xavier2002@yahoo.fr>
2020-04-28 16:06:08 +0200

    stage2

--- creditcalc.py
+++ creditcalc.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import ceil,log
 
 
 # credit_principal = 'Credit principal: 1000'
@@ -9,10 +9,10 @@ from math import ceil
 #
 # print(credit_principal,first_month,second_month,third_month,final_output,sep="\n")
 
-def get_int(msg=""):
+def get_float(msg=""):
     while True:
         try:
-            return int(input(msg))
+            return float(input(msg))
         except:
             print("invalid input")
 
@@ -21,36 +21,71 @@ def get_int(msg=""):
 
 def get_options():
     options="""What do you want to calculate? 
-type "m" - for count of months, 
-type "p" - for monthly payment:
+type "n" - for count of months,
+type "a" - for annuity monthly payment,
+type "p" - for credit principal:
 """
     return input(options)
 
 
-def get_credit_principal():
-    return get_int("Enter the credit   principal: ")
+# def get_credit_principal():
+#     return get_float("Enter the credit   principal: ")
 
 def compute_number_month(credit):
-    payment=get_int("Enter monthly payment: ")
-    number_month=round(credit/payment)
-    return f"It takes {number_month} to repay the credit."
-
-def compute_monthly_payment(credit):
-    number_month=get_int("Enter count of months: ")
-    payment=ceil(credit/number_month)
-    last_payment = credit - payment * (number_month-1)
-    msg = f"Your monthly payment = {payment} "
-    if last_payment > 0:
-        msg += f"with last month payment = {last_payment}"
+    payment=get_float("Enter monthly payment: ")
+    interest = float(input("Enter credit interest: ")) / 1200
+    count  = ceil(log( payment /  ( payment - interest * credit), interest + 1))
+    print(count)
+    years = count // 12
+    months = count  %12
+    msg = "You need "
+    if years > 0 :
+        msg += str(int(years))+f" year{'s' if years > 1 else '' } "
+    if months > 0:
+        if years > 0 :
+            msg += "and "
+        msg += str(ceil(months)) + " months "
+    msg += "to repay this credit!"
     return msg
 
+
+
+def compute_annuity_monthly_payment(credit):
+    periods = int(input("Enter count of periods:"))
+    interest = float(input("Enter credit interest: ")) / 1200
+    annuity = ceil(credit * (interest  * ( 1 + interest) ** periods) / ((1 + interest )**periods -1))
+    return f"Your annuity payment = {annuity}!"
+
+# def compute_monthly_payment(credit):
+#     number_month=get_int("Enter count of months: ")
+#     payment=ceil(credit/number_month)
+#     last_payment = credit - payment * (number_month-1)
+#     msg = f"Your monthly payment = {payment} "
+#     if last_payment > 0:
+#         msg += f"with last month payment = {last_payment}"
+#     return msg
+
+def get_credit_principal():
+    return  float (input("Enter credit principal: "))
+
+def compute_credit_principal():
+    payment = float(input("Enter monthly payment"))
+    periods = int(input("Enter count of periods"))
+    interest  = float(input("Enter credit interest")) /1200
+    credit= payment / (( interest * (1 + interest) ** periods ) / ( (1 + interest) ** periods  - 1))
+    return f"Your credit principal = {credit}!"
+
 def main():
-    credit_principal=get_credit_principal()
     option=get_options()
-    if option=="m":
+    if option=="n":
+        credit_principal = get_credit_principal()
         result=compute_number_month(credit_principal)
-    else:
-        result=compute_monthly_payment(credit_principal)
+    elif option == "a":
+        credit_principal = get_credit_principal()
+        result=compute_annuity_monthly_payment(credit_principal)
+    elif option=="p":
+        result = compute_credit_principal()
+
     print(result)
 
 
